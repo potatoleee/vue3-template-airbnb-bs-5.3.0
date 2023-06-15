d633aaba771127e0bfc6edaeb5064baa12f1d23c
dean <>
2023-06-15 17:55:49 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+// 引入 VeeValidate 元件跟功能
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate';
+// 引入 VeeValidate 的驗證規則
+import AllRules from '@vee-validate/rules';
+// 引入 VeeValidate 的 i18n 功能
+import { localize, setLocale } from '@vee-validate/i18n';
+// 引入 VeeValidate 的繁體中文語系檔
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
+import 'bootstrap';
+import axios from 'axios';
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
+import VueAxios from 'vue-axios';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import router from './router';
+import App from './App.vue';
+
+// 使用 Object.keys 將 AllRules 轉為陣列並使用 forEach 迴圈將驗證規則加入 VeeValidate
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule]);
+});
+
+// 將當前 VeeValidate 的語系設定為繁體中文
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true
+});
+setLocale('zh_TW');
+library.add(fas);
+
+const app = createApp(App);
+// 掛載 Global 的 VeeValidate 元件
+app.use(VueAxios, axios);
+app.component('VField', Field);
+app.component('VForm', Form);
+app.component('ErrorMessage', ErrorMessage);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
---
+++ src/views/IndexView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1>首頁</h1>
+</template>
+
+<style lang="scss">
+@import '@/assets/style/all.scss';
+</style>
